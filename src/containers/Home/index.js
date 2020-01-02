83c0f46d8e3836546fe4aa85482096683d94eb48
Oleksii Kupin <interstates21@gmail.com>
2020-01-02 03:07:08 +0200

    restyle

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -7,7 +7,6 @@ import {bio} from "../../industries";
 
 const data = bio;
 
-
 const Button = ({onClick}) => {
     return (
         <div onClick={onClick} className={classes.redButton}>
@@ -31,12 +30,12 @@ const Home = ({history}) => {
         })
             .then(e => e.json())
             .then(d => {
-                console.log("d == " ,d);
+                console.log("d == ", d);
                 if (d.success) {
                     history.push({
-                        pathname: '/match',
-                        state: { matches: d.data }
-                    })
+                        pathname: "/match",
+                        state: {matches: d.data}
+                    });
                 }
             })
             .catch(error => console.log(error));
@@ -47,32 +46,48 @@ const Home = ({history}) => {
         console.log("token ===", token);
         if (token) {
             sendRequest(token);
-        }
-        else {
-            history.push('/login')
+        } else {
+            history.push("/login");
         }
     };
 
-    const handleChangeSelect = (node) => {
-        console.log("none = ", node)
+    const handleChangeSelect = node => {
+        console.log("none = ", node);
         setSub(node.label);
-    }
+    };
 
     return (
         <div className={classes.home}>
             <div className={classes.half}>
                 <div className={classes.block}>
+                    <p className={classes.text}>
+                        Listing your startup and choosing the right category (or
+                        industry) for your business is an important step in your
+                        fundraising and business development activities. There
+                        are hundreds of industries in the tech world, and dozens
+                        of new industries emerge every year. It’s hard to keep
+                        track of all the changes.
+                            <br/><br/>
+                        This service offers you to check which industries are most frequently mentioned
+                        together with your industry. Please choose the industry
+                        from the dropdown menu to see 5 neighboring industries
+                        (from our database of 500+ categories).
+                    </p>
+                </div>
+            </div>
+            <div className={classes.half}>
+                <div className={classes.pickBlock}>
+                    <h1 className={classes.titleMain}>Pick Your Industry</h1>
                     <IndustryList
                         industry={industry}
                         setIndustry={setIndustry}
                     />
-                </div>
-            </div>
-            <div className={classes.half}>
-                <div className={classes.block}>
-                    <h1 className={classes.titleMain}>Pick Your Industry</h1>
                     <div className={classes.selectContainer}>
-                        <MySelect onChange={handleChangeSelect} data={data} setCallback={setSub}></MySelect>
+                        <MySelect
+                            onChange={handleChangeSelect}
+                            data={data}
+                            setCallback={setSub}
+                        ></MySelect>
                     </div>
                     <Button onClick={handleRequest} />
                 </div>
